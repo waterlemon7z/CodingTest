adb4e76c814d62f807f567d7bb56542b9ed9ad7b
Lemon7z <Lemon7z@icloud.com>
2024-01-22 12:09:34 +0900

    [Bronze IV] Title: 기찍 N, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+//#include <string>
+using namespace std;
+
+int main()
+{
+	string str;
+	int rst;
+	cin >> rst;
+	for(; rst > 0; rst--)
+		cout <<rst << '\n';
+}
