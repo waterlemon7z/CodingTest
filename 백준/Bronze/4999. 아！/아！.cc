31a41f8558e67aa59c2d85f79a2eba9b3e46bea7
Lemon7z <Lemon7z@icloud.com>
2024-01-22 12:11:59 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str1, str2;
+    cin >> str1 >> str2;
+    if(str1.size() >= str2.size())
+        cout << "go";
+    else
+        cout << "no";
+}
