a6032eb1bcf968b14788e6614326811e9a0bb9c5
Lemon7z <Lemon7z@icloud.com>
2024-01-22 12:05:45 +0900

    [Bronze II] Title: 벌집, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <set>
+#include <cmath>
+
+#define INF 987654321
+#define DIV 1000000007
+#define endl '\n'
+#define TIMER_S start = clock();
+#define TIMER_F cout << (clock() - start) << "ms" << endl;
+using namespace std;
+
+//clock_t start;
+typedef pair<int, int> pii;
+typedef unsigned long long ull;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int N;
+	cin >> N;
+	int cur = 1;
+	int plus = 6;
+	int rst =1;
+
+	while(N > cur)
+	{
+		cur += plus;
+		plus += 6;
+		rst++;
+	}
+	cout << rst;
+}
