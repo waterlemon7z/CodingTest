002a6379d73dcab3a68567b92550f15f7f84864f
Lemon7z <Lemon7z@icloud.com>
2024-01-22 12:14:40 +0900

    [Silver IV] Title: 상근이의 여행, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#define INF 987654321
+#define DIV 1000000007
+#define endl '\n'
+#define TIMER_S start = clock();
+#define TIMER_F cout << (clock() - start) << "ms" << endl;
+using namespace std;
+//clock_t start;
+//typedef pair<int, int> pii;
+//typedef unsigned long long ull;
+
+int main()
+{
+	int N, a, b, temp;
+
+	cin >> N;
+	for(int i = 0; i < N; i++)
+	{
+		cin >> a >> b;
+		for(int j = 0; j < b; j++)
+		{
+			cin >> temp >> temp;
+		}
+		cout << a - 1 << endl;
+	}
+}
