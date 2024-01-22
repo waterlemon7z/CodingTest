30a6ce52944ae52122410266aea5d47635175e12
Lemon7z <Lemon7z@icloud.com>
2024-01-22 12:26:27 +0900

    [Bronze V] Title: CAPS, Time: 8 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <set>
+#include <map>
+#include <cmath>
+
+#define INF 987654321
+#define DIV 1000000007
+#define endl '\n'
+#define TIMER_S start = clock();
+#define TIMER_F cout << (clock() - start) << "ms" << endl;
+using namespace std;
+
+//clock_t start;
+typedef pair<int, int> pii;
+typedef unsigned long long ull;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	string str;
+	cin >> str;
+	transform(str.begin(), str.end(), str.begin(), ::toupper);
+	cout << str;
+}
