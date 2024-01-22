6335f39df309e6e3507c00d39beb04ed208a6dde
Lemon7z <Lemon7z@icloud.com>
2024-01-22 12:06:21 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <queue>
+#include <set>
+#include <map>
+#include <cmath>
+
+#define INF 987654321
+#define DIV 1000000007
+#define endl '\n'
+#define TIMER_S start = clock();
+#define TIMER_F cout << (clock() - start) << "ms" << endl;
+using namespace std;
+
+//clock_t start;
+typedef pair<int, int> pii;
+
+typedef unsigned long long ull;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	printf("  ___  ___  ___\n"
+		   "  | |__| |__| |\n"
+		   "  |           |\n"
+		   "   \\_________/\n"
+		   "    \\_______/\n"
+		   "     |     |\n"
+		   "     |     |\n"
+		   "     |     |\n"
+		   "     |     |\n"
+		   "     |_____|\n"
+		   "  __/       \\__\n"
+		   " /             \\\n"
+		   "/_______________\\");
+}
