2fbc3d9c32b15ee8f901c407979bbf2a36454b20
Lemon7z <Lemon7z@icloud.com>
2024-01-22 12:12:40 +0900

    [Bronze V] Title: 카드 게임, Time: 76 ms, Memory: 30852 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.py
@@ -0,0 +1,12 @@
+sum = 0
+a = int(input())
+sum = sum + a
+a = int(input())
+sum = sum + a
+a = int(input())
+sum = sum + a
+a = int(input())
+sum = sum + a
+a = int(input())
+sum = sum + a
+print(sum)
