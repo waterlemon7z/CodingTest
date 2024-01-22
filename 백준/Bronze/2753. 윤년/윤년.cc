b87feb3c26fbe7d07f18dc118c6058bbcfde7fac
Lemon7z <Lemon7z@icloud.com>
2024-01-22 12:10:11 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A, B;
+	cin >> A;
+	if(A % 4 == 0 && A % 100 != 0)
+	{
+		cout << 1;
+	}
+	else
+	{
+		if(A % 400 == 0)
+			cout << 1;
+		else
+			cout << 0;
+	}
+}
