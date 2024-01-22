5f9789a6ed9f4c356de72d359692db20004f9348
Lemon7z <Lemon7z@icloud.com>
2024-01-22 12:13:38 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <queue>
+#include <set>
+#include <map>
+#include <cmath>
+
+#define INF 987654321
+#define DIV 1000000007
+#define endl '\n'
+#define TIMER_S start = clock();
+#define TIMER_F cout << (clock() - start) << "ms" << endl;
+using namespace std;
+
+//clock_t start;
+typedef pair<int, int> pii;
+
+typedef unsigned long long ull;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int T,x ,y;
+	cin >> T;
+	while(T--)
+	{
+		cin >> x >> y;
+		cout << x + y << endl;
+	}
+}
