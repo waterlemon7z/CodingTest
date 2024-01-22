6c29816d25b9af9af5de7119a7a64f374c8b1738
Lemon7z <Lemon7z@icloud.com>
2024-01-22 12:26:37 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 632 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int N,a, b;
+	cin.tie(NULL);
+	cin >> N;
+	for(int i = 0; i < N; i++)
+	{
+		cin >> a >> b;
+		cout << a+ b << '\n';
+	}
+
+}
