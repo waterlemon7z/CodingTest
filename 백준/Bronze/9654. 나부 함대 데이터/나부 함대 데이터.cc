50b905e365a051c5bcc2a5a68b03fbe5a8c0ec51
Lemon7z <Lemon7z@icloud.com>
2024-01-22 12:15:12 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <vector>
+#define INF 987654321
+#define DIV 1000000007
+#define endl '\n'
+#define TIMER_S start = clock();
+#define TIMER_F cout << (clock() - start) << "ms" << endl;
+using namespace std;
+//clock_t start;
+//typedef pair<int, int> pii;
+int n;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n"
+			"N2 Bomber      Heavy Fighter  Limited    21        \n"
+			"J-Type 327     Light Combat   Unlimited  1         \n"
+			"NX Cruiser     Medium Fighter Limited    18        \n"
+			"N1 Starfighter Medium Fighter Unlimited  25        \n"
+			"Royal Cruiser  Light Combat   Limited    4         ";
+}
