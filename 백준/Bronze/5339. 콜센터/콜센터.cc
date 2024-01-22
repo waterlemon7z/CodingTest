4a02d817c938af3a54d79d9d6b0fc3496040510b
Lemon7z <Lemon7z@icloud.com>
2024-01-22 12:12:25 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	cout << "     /~\\\n"
+			"    ( oo|\n"
+			"    _\\=/_\n"
+			"   /  _  \\\n"
+			"  //|/.\\|\\\\\n"
+			" ||  \\ /  ||\n"
+			"============\n"
+			"|          |\n"
+			"|          |\n"
+			"|          |";
+}
