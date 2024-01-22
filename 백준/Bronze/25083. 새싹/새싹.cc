7af8f37f0168f783f76a66d9d292d27bc717bcdd
Lemon7z <Lemon7z@icloud.com>
2024-01-22 12:30:46 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string str("         ,r'\"7\n"
+			   "r`-_   ,'  ,/\n"
+			   " \\. \". L_r'\n"
+			   "   `~\\/\n"
+			   "      |\n"
+			   "      |");
+	cout << str;
+}
