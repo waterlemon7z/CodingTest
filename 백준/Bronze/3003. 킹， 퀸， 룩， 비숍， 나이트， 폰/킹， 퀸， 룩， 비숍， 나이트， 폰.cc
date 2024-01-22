676e02c16d8c50c31117e9787ab388873bb7d2cf
Lemon7z <Lemon7z@icloud.com>
2024-01-22 12:11:02 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+	vector<int> arr;
+	int A, B, C, d, e, f;
+	cin >> A >> B >> C >> d >> e >> f;
+	cout << 1 - A << ' ' << 1 - B<< ' ' << 2 - C<< ' '  << 2 - d<< ' ' << 2 - e<< ' ' << 8 - f;
+}
