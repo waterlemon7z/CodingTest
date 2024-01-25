c8ec295be3986e3a1aa022c4b456b2f8ac36cbf2
Lemon7z <Lemon7z@icloud.com>
2024-01-25 21:11:18 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define INF 2147483647
+#define DIV 1000000007
+#define endl '\n'
+#define TIMER_S start = clock();
+#define TIMER_F cout << (clock() - start) << "ms" << endl;
+#define FASTIO ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+using namespace std;
+
+typedef pair<int, int> pii;
+typedef unsigned long long ull;
+typedef long long ll;
+
+
+int main()
+{
+	FASTIO
+	ll n,m;
+	cin >> n >> m;
+	cout << (n==m ? 1 : 0);
+}
