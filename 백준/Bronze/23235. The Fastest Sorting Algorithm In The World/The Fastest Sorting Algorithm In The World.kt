11324b34848d07d7e0949574b93a13100094159a
Lemon7z <Lemon7z@icloud.com>
2024-02-01 20:36:02 +0900

    [Bronze V] Title: The Fastest Sorting Algorithm In The World, Time: 128 ms, Memory: 15028 KB -BaekjoonHub

---
+++ 백준/Bronze/23235. The Fastest Sorting Algorithm In The World/The Fastest Sorting Algorithm In The World.kt
@@ -0,0 +1,11 @@
+fun main()
+{
+    var cnt = 1
+    while(true)
+    {
+        val str = readln()
+        if(str[0] == '0')
+            break;
+        println("Case ${cnt++}: Sorting... done!")
+    }
+}
