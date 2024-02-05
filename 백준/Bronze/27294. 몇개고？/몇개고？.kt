6d08b3939980b7d6d28d8ba022d0c5f718e94b1c
Lemon7z <Lemon7z@icloud.com>
2024-02-05 15:55:44 +0900

    [Bronze V] Title: 몇개고?, Time: 144 ms, Memory: 20904 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.kt
@@ -0,0 +1,12 @@
+fun main()
+{
+    val (N, S) = readln().split(" ").map{it.toInt()}
+    if(N in 12..16)
+    {
+        if(S == 1)
+            println(280)
+else        println("320")
+    }
+    else
+    println("280")
+}
